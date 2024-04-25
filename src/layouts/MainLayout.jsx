838da5c83660d2da4b256cfc65e27702c4739394
itsahadul99 <itsahadul999@gmail.com>
2024-04-25 20:36:56 +0600

    added nav bar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Nav from "../components/shayred/Nav";
 
 const MainLayout = () => {
     return (
         <div>
+            <Nav />
             <Outlet />
         </div>
     );
